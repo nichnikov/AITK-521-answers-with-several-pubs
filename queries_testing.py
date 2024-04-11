e25a1b17ac4bff674fc4687e95a7305d7aa8c084
nichnikov <a.nichnikov@gmail.com>
2024-04-11 16:58:51 +0300

    ref and data

--- queries_testing.py
+++ queries_testing.py
@@ -12,18 +12,29 @@ print(queies_df.info())
 queies_dicts = queies_df.to_dict(orient="records")
 shuffle(queies_dicts)
 
-test_quantity = 1118
+test_quantity = 2688
 
 test_results = []
 for num, d in enumerate(queies_dicts[:test_quantity]):
-    print(num + 1, "/", test_quantity)
-    # q = d["Query"]
-    q_request = {"pubid": 9, "text": re.sub("\n", " ", d["Query"])}
-    res = requests.post("http://0.0.0.0:8090/api/search", json=q_request)
-    # res = requests.post("http://srv01.nlp.dev.msk2.sl.amedia.tech:4011/api/search", json=q_request)
-    res_dict = {**d, **res.json()}
-    test_results.append(res_dict)
+    try:
+        temp_results = {}
+        print(num + 1, "/", test_quantity)
+        for pubid in [9, 16, 17, 25]:
+            # q = d["Query"]
+            q_request = {"pubid": pubid, "text": re.sub("\n", " ", d["Query"])}
+            res = requests.post("http://0.0.0.0:8090/api/search", json=q_request)
+            res_d = res.json()
+            res_d_with_pub = {}
+            for name in ["templateId", "templateText", "sbert_best_id", "sbert_best_etalon", 
+                        "sbert_best_answer", "sbert_score", "T5Opinion", "T5Score"]:
+                res_d_with_pub["".join([name, "_", str(pubid)])] = res_d[name]
+            # res = requests.post("http://srv01.nlp.dev.msk2.sl.amedia.tech:4011/api/search", json=q_request)
+            temp_results = {**temp_results, **d, **res_d_with_pub}
+        test_results.append(temp_results)
+    except:
+        pass
 
 test_results_df = pd.DataFrame(test_results)
+
 print(test_results_df)
-test_results_df.to_csv(os.path.join("results", "exp_supp_testing_240410.csv"), sep="\t", index=False)
+test_results_df.to_csv(os.path.join("results", "exp_supp_testing_pubs240411.csv"), sep="\t", index=False)
